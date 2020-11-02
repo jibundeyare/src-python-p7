d29243abfc6526c6171d0f85f6e9175410e0239d
Jibundeyare <contact@jibundeyare.com>
2020-11-02 12:48:36 +0100

    Amélioration des commentaires

--- dicts.py
+++ dicts.py
@@ -27,29 +27,39 @@ print(my_dict)
 # - None
 # - tuple
 
+# ajouter un nouveau couple clé / valeur
 my_dict['ipsum'] = 'lorem ipsum'
 print(my_dict)
 
+# modifier la valeur associée à une clé existante
 my_dict['lorem'] = 'sit dolores'
 print(my_dict)
 
+# supprimer un couple clé / valeur
 del(my_dict['ipsum'])
 print(my_dict)
 
+# afficher les clés
 for key in my_dict:
     print(key)
 
+# afficher les valeurs
 for key in my_dict:
     print(my_dict[key])
 
+# afficher les clés et les valeurs
 for key in my_dict:
     print(f"{key}: {my_dict[key]}")
 
+# afficher les clés et les valeurs
+# méthode alternative
 for key, value in my_dict.items():
     print(f"{key}: {value}")
 
+# récupérer la liste des clés
 my_keys = list(my_dict.keys())
 print(my_keys)
 
+# récupérer la liste des valeurs
 my_values = list(my_dict.values())
 print(my_values)
