a59676b88beaceb5d6989eacfc0ca0d482328a3d
Jibundeyare <contact@jibundeyare.com>
2020-11-04 19:04:28 +0100

    Add code

--- classes.py
+++ classes.py
@@ -1,14 +1,99 @@
 class CarFoo:
     def __init__(self):
         """Constructeur de la classe CarFoo"""
+        # self désigne l'instance de la classe (ou objet)
         # self == this
-        # attribut == variable rattachée à une instance de classe / un objet
+        # attribut == variable rattachée à une instance de classe (ou objet)
         self.wheels = 4
         self.engine_on = False
         self.seats = 4
         self.passengers = 0
 
-# instance d'une classe == objet
+class CarBar:
+    def __init__(self):
+        """Constructeur de la classe CarBar"""
+        # un _ devant le nom d'un attribut == attribut privé
+        self._wheels = 4
+        self._engine_on = False
+        self._seats = 4
+        self._passengers = 0
+
+    # méthode == fonction dans une classe
+    # un getter == une méthode qui permet de récupérer la valeur d'un attribut
+    # un setter == une méthode qui permet de modifier la valeur d'un attribut
+
+    # getter pour l'attribut _wheels
+    def get_wheels():
+        return self._wheels
+
+    # pas besoin de setter pour l'attribut _wheels
+    # car le nombre de roues ne change jamais
+
+    # getter pour l'attribut _engine_on
+    # les méthodes qui renvoient un booléen, peuvent être préfixées d'un `is_` ou d'un `has_` au lieu d'un `get_`
+    def is_engine_on(self):
+        return self._engine_on
+
+    # setter pour l'attribut _engine_on qui permet d'allumer ou éteindre le moteur
+    def set_engine_on(self, engine_on):
+        if type(engine_on) is bool:
+            self._engine_on = engine_on
+
+    # setter pour l'attribut _engine_on qui permet seulement d'allumer le moteur
+    def start_engine(self):
+        self._engine_on = True
+
+    # setter pour l'attribut _engine_on qui permet seulement d'éteindre le moteur
+    def stop_engine(self):
+        self._engine_on = False
+
+    # setter pour l'attribut _seats
+    def get_seats():
+        return self._wheels
+
+    # pas besoin de setter pour l'attribut _seats
+    # car le nombre de sièges ne change jamais
+
+    # getter qui calcule à la volée le nombre de places libres
+    def get_free_seats(self):
+        return self._seats - self._passengers
+
+    # pas besoin de setter pour le nombre de places libres
+    # car n'est pas un attribut
+
+    # getter pour l'attribut _passengers
+    def get_passengers(self):
+        return self._passengers
+
+    # ce setter « basique » pour l'attribut _passengers
+    # n'est pas utile car il ne permet pas de choisir
+    # le nombre de passagers en fonction des places disponibles
+    # def set_passengers(self, passengers):
+    #     self._passengers = passengers
+
+    # ce setter « personnalisé » pour l'attribut _passengers
+    # permet d'ajouter des passagers en fonction des places disponibles
+    def add_passengers(self, passengers):
+        if passengers > self.get_free_seats():
+            self._passengers += self.get_free_seats()
+        else:
+            self._passengers += passengers
+
+    # ce deuxième setter « personnalisé » pour l'attribut _passengers
+    # permet de retirer des passagers en fonction des passagers du véhicule
+    def remove_passengers(self, passengers):
+        if passengers > self._passengers:
+            self.all_passengers_off()
+        else:
+            self._passengers -= passengers
+
+    # ce troisième setter « personnalisé » pour l'attribut _passengers
+    # permet de faire décendre tout le monde sans se préoccuper
+    # du nombre de passagers du véhicule
+    def all_passengers_off():
+        self._passengers = 0
+
+# instance d'une classe == objet == une variable contenant une valeur du type de la classe
 v1 = CarFoo()
 print(v1.wheels)
 print(v1.engine_on)
@@ -29,61 +114,6 @@ print()
 
 print(v2.engine_on)
 print()
-
-class CarBar:
-    def __init__(self):
-        """Constructeur de la classe CarBar"""
-        # un _ devant le nom d'un attribut == attribut privé
-        self._wheels = 4
-        self._engine_on = False
-        self._seats = 4
-        self._passengers = 0
-
-    # un getter == une méthode qui permet de récupérer la valeur d'un attribut
-    # un setter == une méthode qui permet de modifier la valeur d'un attribut
-
-    # méthode == fonction dans une classe
-    def get_wheels():
-        return self._wheels
-
-    def set_engine_on(self, engine_on):
-        if type(engine_on) is bool:
-            self._engine_on = engine_on
-
-    def start_engine(self):
-        self._engine_on = True
-
-    def stop_engine(self):
-        self._engine_on = False
-
-    # les méthodes qui renvoient un booléen, peuvent être préfixées d'un `is_` ou d'un `has_` au lieu d'un `get_`
-    def is_engine_on(self):
-        return self._engine_on
-
-    def get_seats():
-        return self._wheels
-
-    def get_free_seats(self):
-        return self._seats - self._passengers
-
-    def get_passengers(self):
-        return self._passengers
-
-    # cette méthode @todo
-    # def set_passengers(self, passengers):
-    #     self._passengers = passengers
-
-    def add_passengers(self, passengers):
-        if passengers > self.get_free_seats():
-            self._passengers += self.get_free_seats()
-        else:
-            self._passengers += passengers
-
-    def remove_passengers(self, passengers):
-        pass
-
-    def all_passengers_off():
-        self._passengers = 0
 
 v3 = CarBar()
 v3.start_engine()
